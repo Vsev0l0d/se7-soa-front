5703ad0a6e41559fb1831330c5c3796209e0eb0d
Vsev0l0d <vsevolod628@gmail.com>
2022-10-12 22:33:47 +0300

    s

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import {ModalWindow} from './components/ModalWindow'
 import {RecoilRoot} from 'recoil'
 import {AddRouteButton} from './components/AddRouteButton'
 import {DeleteRouteButton} from './components/DeleteRouteButton'
+import {QuantityFormByDistance} from './components/QuantityFormByDistance'
 
 function App() {
 	return (
 		<RecoilRoot>
 			<div className="container pt-4">
+				<QuantityFormByDistance/>
 				<AddRouteButton/>
 				<DeleteRouteButton/>
 				<ModalWindow/>
---
+++ src/components/QuantityFormByDistance.js
@@ -0,0 +1,25 @@
+import {Form, InputGroup} from 'react-bootstrap'
+import Button from 'react-bootstrap/Button'
+import {useState} from 'react'
+
+export const QuantityFormByDistance = () => {
+	const [distance, setDistance] = useState('')
+
+	const findOut = () => {
+		alert(document.getElementById("selectComparisonOperation").value + " " + distance)
+	}
+
+	return (
+		<Form>
+			<InputGroup className="mb-3">
+				<Button variant="dark" onClick={findOut} disabled={distance === ''}>Find out</Button>
+				<InputGroup.Text> the number of objects whose distance field value</InputGroup.Text>
+				<Form.Select id="selectComparisonOperation">
+					<option value="equals">is</option>
+					<option value="greater">is greater than</option>
+				</Form.Select>
+				<Form.Control type="number" onChange={event => setDistance(event.target.value)}/>
+			</InputGroup>
+		</Form>
+	)
+}
